9955202f2e4f8024aabd591a0b6ab3cf4e4751f5
dannychoi1130 <158004162+dannychoi1130@users.noreply.github.com>
2024-03-26 16:00:51 +0800

    try

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import * as React from "react";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TopBar />
+    </div>
+  );
 }
 
 export default App;
